5d909386b27e057d40a0c3dc1b7fb11c682aaccb
s34rching <ivan.katsenya@gmail.com>
2017-06-28 19:15:03 +0300

    Parameters were added to methods + new test - tests passed

--- test_add_group.py
+++ test_add_group.py
@@ -18,14 +18,14 @@ class test_add_group(unittest.TestCase):
     def open_home_pafe(self, wd):
         wd.get("http://localhost/addressbook/")
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def open_group_page(self, wd):
@@ -34,17 +34,17 @@ class test_add_group(unittest.TestCase):
     def init_group_creation(self, wd):
         wd.find_element_by_name("new").click()
 
-    def create_group(self, wd):
+    def create_group(self, wd, group_name, header, footer):
         # fill creating form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("test_gruop")
+        wd.find_element_by_name("group_name").send_keys(group_name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("some_text")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("some_text")
+        wd.find_element_by_name("group_footer").send_keys(footer)
         # submit group cretion
         wd.find_element_by_name("submit").click()
 
@@ -57,10 +57,20 @@ class test_add_group(unittest.TestCase):
     def test_test_add_group(self):
         wd = self.wd
         self.open_home_pafe(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_group_page(wd)
         self.init_group_creation(wd)
-        self.create_group(wd)
+        self.create_group(wd, group_name="test_group", header="some_text", footer="some_text")
+        self.return_to_group_page(wd)
+        self.logout(wd)
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_pafe(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_group_page(wd)
+        self.init_group_creation(wd)
+        self.create_group(wd, group_name="", header="", footer="")
         self.return_to_group_page(wd)
         self.logout(wd)
 
