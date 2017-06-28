b937392857781977ddaf353578bc607b5b953864
s34rching <ivan.katsenya@gmail.com>
2017-06-28 22:08:44 +0300

    Additional class was added

--- test_add_group.py
+++ test_add_group.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import unittest
+from group import Group
+
 
 def is_alert_present(wd):
     try:
@@ -34,17 +35,17 @@ class test_add_group(unittest.TestCase):
     def init_group_creation(self, wd):
         wd.find_element_by_name("new").click()
 
-    def create_group(self, wd, group_name, header, footer):
+    def create_group(self, wd, group):
         # fill creating form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group_name)
+        wd.find_element_by_name("group_name").send_keys(group.group_name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group cretion
         wd.find_element_by_name("submit").click()
 
@@ -60,7 +61,7 @@ class test_add_group(unittest.TestCase):
         self.login(wd, username="admin", password="secret")
         self.open_group_page(wd)
         self.init_group_creation(wd)
-        self.create_group(wd, group_name="test_group", header="some_text", footer="some_text")
+        self.create_group(wd, Group(group_name="test_group", header="some_text", footer="some_text"))
         self.return_to_group_page(wd)
         self.logout(wd)
 
@@ -70,7 +71,7 @@ class test_add_group(unittest.TestCase):
         self.login(wd, username="admin", password="secret")
         self.open_group_page(wd)
         self.init_group_creation(wd)
-        self.create_group(wd, group_name="", header="", footer="")
+        self.create_group(wd, Group(group_name="", header="", footer=""))
         self.return_to_group_page(wd)
         self.logout(wd)
 
