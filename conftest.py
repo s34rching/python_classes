db127e58b0e6186d84e53031110d22f4462ccce9
s34rching <ivan.katsenya@gmail.com>
2017-07-29 18:45:23 +0300

    Command line options were implemented

--- conftest.py
+++ conftest.py
@@ -6,11 +6,13 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption('--browser')
+    baseUrl = request.config.getoption('--baseUrl')
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, baseUrl=baseUrl)
     else:
         if not fixture.is_valid:
-            fixture = Application()
+            fixture = Application(browser=browser, baseUrl=baseUrl)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -21,3 +23,7 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(final)
     return fixture
+
+def pytest_addoption(parser):
+    parser.addoption('--browser', action='store', default='firefox')
+    parser.addoption('--baseUrl', action='store', default="http://localhost/addressbook/")
